8e10f38c128bc6b846cd660feab06b7239a1444d
akey <darkmark404@protonmail.com>
2026-01-26 23:45:22 -0700

    test (build.rs): build rust proto buff

---
+++ build.rs
@@ -0,0 +1,24 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let proto_files = &[
+        "proto/types.proto",
+        "proto/build.proto",
+        "proto/data_stream.proto",
+        "proto/execution.proto",
+        "proto/introspection.proto",
+        "proto/security.proto",
+        "proto/system_utils.proto",
+        "proto/file_system.proto",
+        "proto/settings.proto",
+        "proto/terminal.proto",
+        "proto/workspace.proto",
+    ];
+
+    let proto_dirs = &["proto"];
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .compile(proto_files, proto_dirs)?;
+
+    Ok(())
+}
